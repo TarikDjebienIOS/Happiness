bea8b33c1c347e0964efb010349f745e65d701a4
Tarik Djebien <tarik@MacBookPro.local>
2013-04-26 14:31:59 +0200

    Initial Commit

---
+++ Happiness/HappinessViewController.h
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.h
+//  Happiness
+//
+//  Created by Tarik Djebien on 26/04/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HappinessViewController : UIViewController
+
+@end
